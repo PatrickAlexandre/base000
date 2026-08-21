6a9caeeb25b30da2bc208c8c796815e97ccd8926
PatrickAlexandre <158535638+PatrickAlexandre@users.noreply.github.com>
2026-08-21 18:34:05 +0200

    Stop loading DOM enhancer scripts

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-import './formation.css'
-import './formation-enhancer.js'
-import './money.css'
-import './money-enhancer.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

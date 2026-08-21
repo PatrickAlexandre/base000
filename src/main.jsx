b51f64da309c0743444178eeab61b0dd4f563a53
PatrickAlexandre <158535638+PatrickAlexandre@users.noreply.github.com>
2026-08-21 06:09:13 +0200

    Load Argent module assets

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './styles.css'
 import './formation.css'
 import './formation-enhancer.js'
+import './money.css'
+import './money-enhancer.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
